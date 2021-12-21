d9a54ce8244630530819e090a0d8da0acf7975de
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-12-21 23:59:43 +0100

    add more methods

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,57 @@ impl KeyPair {
     pub fn new() -> KeyPair {
         KeyPair(biscuit::KeyPair::new())
     }
+
+    pub fn public(&self) -> PublicKey {
+        PublicKey(self.0.public())
+    }
+
+    pub fn private(&self) -> PrivateKey {
+        PrivateKey(self.0.private())
+    }
 }
+
+/// Public key
+#[wasm_bindgen]
+pub struct PublicKey(biscuit::PublicKey);
+
+#[wasm_bindgen]
+impl PublicKey {
+    pub fn to_bytes(&self, out: &mut [u8]) -> Result<(), JsValue> {
+        if out.len() != 32 {
+            return Err("invalid length".into());
+        }
+
+        out.copy_from_slice(&self.0.to_bytes());
+        Ok(())
+    }
+
+    pub fn from_bytes(&self, data: &[u8]) -> Result<PublicKey, JsValue> {
+        let key = biscuit_auth::PublicKey::from_bytes(data).map_err(|e| e.to_string())?;
+        Ok(PublicKey(key))
+    }
+}
+#[wasm_bindgen]
+pub struct PrivateKey(biscuit::PrivateKey);
+
+#[wasm_bindgen]
+impl PrivateKey {
+    pub fn to_bytes(&self, out: &mut [u8]) -> Result<(), JsValue> {
+        if out.len() != 32 {
+            return Err("invalid length".into());
+        }
+
+        out.copy_from_slice(&self.0.to_bytes());
+        Ok(())
+    }
+
+    pub fn from_bytes(&self, data: &[u8]) -> Result<PrivateKey, JsValue> {
+        let key = biscuit_auth::PrivateKey::from_bytes(data).map_err(|e| e.to_string())?;
+        Ok(PrivateKey(key))
+    }
+}
+
+
 
 #[wasm_bindgen]
 extern "C" {
