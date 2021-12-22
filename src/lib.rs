36b89c2bbc3db35872c99d1defcc51d588ec016a
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-12-22 19:04:18 +0100

    builders and more methods

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,111 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
+pub struct Biscuit(biscuit::Biscuit);
+
+#[wasm_bindgen]
+impl Biscuit {
+    pub fn builder() -> BiscuitBuilder {
+        BiscuitBuilder::new()
+    }
+
+    pub fn create_block(&self) -> BlockBuilder {
+        BlockBuilder(self.0.create_block())
+    }
+
+    pub fn append(&self, block: BlockBuilder) -> Result<Biscuit, JsValue> {
+        Ok(Biscuit(self.0.append(block.0).map_err(|e| e.to_string())?))
+    }
+
+    pub fn seal(&self) -> Result<Biscuit, JsValue> {
+        Ok(Biscuit(self.0.seal().map_err(|e| e.to_string())?))
+    }
+
+    pub fn from_bytes(data: &[u8], root: &PublicKey) -> Result<Biscuit, JsValue> {
+        Ok(Biscuit(biscuit::Biscuit::from(data, |_| root.0).map_err(|e| e.to_string())?))
+    }
+
+    pub fn from_base64(data: &str, root: &PublicKey) -> Result<Biscuit, JsValue> {
+        Ok(Biscuit(biscuit::Biscuit::from_base64(data, |_| root.0).map_err(|e| e.to_string())?))
+    }
+
+    pub fn to_bytes(&self) -> Result<Box<[u8]>, JsValue> {
+        Ok(self.0.to_vec().map_err(|e| e.to_string())?.into_boxed_slice())
+    }
+
+    pub fn to_base64(&self) -> Result<String, JsValue> {
+        Ok(self.0.to_base64().map_err(|e| e.to_string())?)
+    }
+}
+
+#[wasm_bindgen]
+pub struct BiscuitBuilder {
+    facts: Vec<biscuit::builder::Fact>,
+    rules: Vec<biscuit::builder::Rule>,
+    checks: Vec<biscuit::builder::Check>,
+}
+
+#[wasm_bindgen]
+impl BiscuitBuilder {
+    fn new() -> BiscuitBuilder {
+        BiscuitBuilder {
+            facts: Vec::new(),
+            rules: Vec::new(),
+            checks: Vec::new(),
+        }
+    }
+
+    pub fn build(self, root: &KeyPair) -> Result<Biscuit, JsValue> {
+        let mut builder = biscuit_auth::Biscuit::builder(&root.0);
+        for fact in self.facts.into_iter() {
+            builder.add_authority_fact(fact).map_err(|e| e.to_string())?;
+        }
+        for rule in self.rules.into_iter() {
+            builder.add_authority_rule(rule).map_err(|e| e.to_string())?;
+        }
+        for check in self.checks.into_iter() {
+            builder.add_authority_check(check).map_err(|e| e.to_string())?;
+        }
+
+
+        Ok(Biscuit(builder.build().map_err(|e| e.to_string())?))
+    }
+
+    pub fn add_authority_fact(&mut self, fact: &str) -> Result<(), JsValue> {
+        self.facts.push(fact.try_into().map_err(|e: biscuit::error::Token| e.to_string())?);
+        Ok(())
+    }
+
+    pub fn add_authority_rule(&mut self, rule: &str) -> Result<(), JsValue> {
+        self.rules.push(rule.try_into().map_err(|e: biscuit::error::Token| e.to_string())?);
+        Ok(())
+    }
+
+    pub fn add_authority_check(&mut self, check: &str) -> Result<(), JsValue> {
+        self.checks.push(check.try_into().map_err(|e: biscuit::error::Token| e.to_string())?);
+        Ok(())
+    }
+}
+
+#[wasm_bindgen]
+pub struct BlockBuilder(biscuit::builder::BlockBuilder);
+
+#[wasm_bindgen]
+impl BlockBuilder {
+    pub fn add_fact(&mut self, fact: &str) -> Result<(), JsValue> {
+        Ok(self.0.add_fact(fact).map_err(|e: biscuit::error::Token| e.to_string())?)
+    }
+
+    pub fn add_rule(&mut self, rule: &str) -> Result<(), JsValue> {
+        Ok(self.0.add_rule(rule).map_err(|e: biscuit::error::Token| e.to_string())?)
+    }
+
+    pub fn add_check(&mut self, check: &str) -> Result<(), JsValue> {
+        Ok(self.0.add_check(check).map_err(|e: biscuit::error::Token| e.to_string())?)
+    }
+}
+
+#[wasm_bindgen]
 pub struct KeyPair(biscuit::KeyPair);
 
 #[wasm_bindgen]
