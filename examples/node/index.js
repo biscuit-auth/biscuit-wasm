f8e702bbafc0991de029667e8740d838cda86dd4
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-12-30 18:17:22 +0100

    clean up the example

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,19 +1,16 @@
-//import init, {Biscuit} from '@biscuit-auth/biscuit-wasm';
-//import pkg from '@biscuit-auth/biscuit-wasm';
-//const {Biscuit} = pkg;
 const {Biscuit, KeyPair} = require('@biscuit-auth/biscuit-wasm');
 
 console.log("a");
 console.log("b");
 
-var builder = Biscuit.builder();
+let builder = Biscuit.builder();
 console.log(JSON.stringify(builder));
 builder.add_authority_fact("user(1234)");
 
 console.log("c");
 
-var root = new KeyPair();
-var token = builder.build(root);
+let root = new KeyPair();
+let token = builder.build(root);
 console.log(token);
 
 var authorizer = token.authorizer();
