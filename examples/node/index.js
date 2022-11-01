04f3952874927f333d185bf89aa231d91d23ed01
Geoffroy Couprie <contact@geoffroycouprie.com>
2022-11-01 19:03:41 +0100

    experiment moving completely to esm

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,10 +1,14 @@
-const {Biscuit, KeyPair, Fact} = require('@biscuit-auth/biscuit-wasm');
+import  {Biscuit, PrivateKey, KeyPair, Fact} from '@biscuit-auth/biscuit-wasm';
 
 console.log("a");
 console.log("b");
 
 let builder = Biscuit.builder();
 console.log(JSON.stringify(builder));
+
+let pk = PrivateKey.from_hex("473b5189232f3f597b5c2f3f9b0d5e28b1ee4e7cce67ec6b7fbf5984157a6b97");
+//let root = new KeyPair();
+let root = KeyPair.from(pk);
 
 //builder.add_authority_fact("user(1234)");
 let fact = Fact.from_str("user($id)")
@@ -14,7 +18,6 @@ fact.set("id", 1234)
 
 console.log("c");
 
-let root = new KeyPair();
 let token = builder.build(root.private());
 console.log(token);
 
