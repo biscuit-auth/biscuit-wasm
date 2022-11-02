a8233da5e5a0afffc24844d783664b3b589f0284
Geoffroy Couprie <contact@geoffroycouprie.com>
2022-11-02 12:53:15 +0100

    frontend example with webpack

---
+++ examples/frontend/index.js
@@ -0,0 +1,31 @@
+import  {Biscuit, PrivateKey, KeyPair, Fact} from '@biscuit-auth/biscuit-wasm';
+
+console.log("a");
+console.log("b");
+
+let builder = Biscuit.builder();
+console.log(JSON.stringify(builder));
+
+let priv = new KeyPair();
+
+let pk = PrivateKey.from_hex("473b5189232f3f597b5c2f3f9b0d5e28b1ee4e7cce67ec6b7fbf5984157a6b97");
+//let root = new KeyPair();
+let root = KeyPair.from(pk);
+
+//builder.add_authority_fact("user(1234)");
+let fact = Fact.from_str("user($id)")
+console.log("bb")
+
+fact.set("id", 1234)
+
+console.log("c");
+
+let token = builder.build(root.private());
+console.log(token);
+
+var authorizer = token.authorizer();
+
+authorizer.add_fact(fact)
+authorizer.add_code("allow if user(1234); deny if true;");
+var policy = authorizer.authorize();
+console.log("policy: "+policy);
