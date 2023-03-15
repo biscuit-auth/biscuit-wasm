a5862323b4249ac8a17bcea44680231fe9769999
Clement Delafargue <clement@delafargue.name>
2023-03-15 23:21:51 +0100

    add tagged template for block builders

--- snippets/tagged-templates.js
+++ snippets/tagged-templates.js
@@ -46,6 +46,42 @@ export function biscuit(strings, ...values) {
   return builder;
 }
 
+export function block(strings, ...values) {
+  let code = "";
+  for (let i = 0; i < strings.length; i++) {
+    code += strings[i];
+    if(i < values.length){
+      code += `{_param_${i}}`
+    } 
+  }
+
+  const termParameters = Object.fromEntries(values.map((v, i) => {
+    return [
+      `_param_${i}`,
+      prepareTerm(v)
+    ];
+  }));
+
+  const isKeyParam = (v) => {
+    return typeof v === "string" && v.startsWith("ed25519/")  || v.toDatalogParameter;
+  };
+
+  const keyParameters = Object.fromEntries(
+    values.map((v,i) => [i,v])
+          .filter(([i,v]) => isKeyParam(v))
+          .map(([i,v]) => {
+            return [
+              `_param_${i}`,
+              prepareTerm(v)
+            ];
+          })
+  );
+
+  const builder = Biscuit.block_builder();
+  builder.addCodeWithParameters(code, termParameters, keyParameters);
+  return builder;
+}
+
 export function authorizer(strings, ...values) {
   let code = "";
   for (let i = 0; i < strings.length; i++) {
